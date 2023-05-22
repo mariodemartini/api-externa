60f414c8cda061f6858d8adcdc74233c14e27432
Mario Martini <mariodemartini@gmail.com>
2023-05-22 09:49:45 -0300

    :tada: initial commit

---
+++ src/main/java/br/com/gd/apiexterna/ApiExternaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gd.apiexterna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExternaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExternaApplication.class, args);
+	}
+
+}
