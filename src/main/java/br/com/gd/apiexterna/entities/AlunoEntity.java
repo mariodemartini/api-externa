7a3271c8a51931aa30baa5880ffd0e2773fb210f
Mario Martini <mariodemartini@gmail.com>
2023-05-22 14:19:17 -0300

    :sparkles: feat: criando entidade

---
+++ src/main/java/br/com/gd/apiexterna/entities/AlunoEntity.java
@@ -0,0 +1,22 @@
+package br.com.gd.apiexterna.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AlunoEntity {
+
+    @Id
+    private String id;
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String endereco;
+
+}
